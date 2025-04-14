cfd69b7eceafaa9d410bc88b46755f93c693949c
Bora AKGUL <borakgul1@gmail.com>
2025-04-14 22:59:47 +0300

    Add product listing components and implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
- import Header from './components/Header';
- import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
  
  const App = () => {
    return (
@@ -8,7 +9,7 @@ import { Container } from 'react-bootstrap';
        <Header />
        <main className='py-3'>
          <Container>
-           <h1>Welcome to ProShop</h1>
+           <Outlet />        
          </Container>
        </main>
        <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,29 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
  import { FaUser, FaBeer } from 'react-icons/fa';
  import  Logo  from '../assets/logo.png';
+ import {LinkContainer} from 'react-router-bootstrap';
  
  const Header = () => {
-   return (
-     <header>
-       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-         <Container>
-           <Navbar.Brand href='/'>
-           <img src={Logo} alt="ProShopp" />
-           ProShop
-           </Navbar.Brand>
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+        <LinkContainer to='/'>
+             <Navbar.Brand>ProShop</Navbar.Brand>
+           </LinkContainer>
            <Navbar.Toggle aria-controls='basic-navbar-nav' />
            <Navbar.Collapse id='basic-navbar-nav'>
              <Nav className='ms-auto'>
-               <Nav.Link href='/cart'>
-                 <FaBeer /> Cart
-               </Nav.Link>
-               <Nav.Link href='/login'>
-                 <FaUser /> Sign In
-               </Nav.Link>
+             <LinkContainer to='/cart'>
+                 <Nav.Link>
+                   <FaBeer /> Cart
+                 </Nav.Link>
+               </LinkContainer>
+               <LinkContainer to='/login'>
+                 <Nav.Link href='/login'>
+                   <FaUser /> Sign In
+                 </Nav.Link>
+               </LinkContainer>
              </Nav>
            </Navbar.Collapse>
          </Container>
