2d55867f299f58d26b5e8dde20806efedc8f8934
Bora Akgül <borakgul1@gmail.com>
2025-04-16 16:14:48 +0300

    Connecting frontend-backend and changing some config

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 import express from 'express';
-import dotenv from 'dotenv'; // Import dotenv to load environment variables
-dotenv.config(); // Load environment variables from .env file
-import products from './data/products.js';  
-const port = process.env.PORT || 5000; // Port number for the server
-const app = express(); // Create an instance of the Express application
-
-app.get('/', (req, res) => { 
-    res.send('Hello World!'); // Send a response to the client  
-});
-
-app.get('/api/products', (req, res) => { 
-    res.json(products); // Send the products data as JSON response
-
-}); 
-app.get('/api/products/:id', (req, res) => { 
-    const product = products.find((p) => p._id === req.params.id); // Find the product by ID
-    res.json(product); // Send the product data as JSON response
-}); // Define a route for the root URL
-
-app.listen(port, () => console.log(`Server is running on port ${port}`)); // Start the server and listen on the specified port
+ import products from './data/products.js';
+ const port = process.env.PORT || 5000;
+ 
+ const app = express();
+ 
+ app.get('/api/products', (req, res) => {
+   res.json(products);
+ });
+ 
+ app.get('/api/products/:id', (req, res) => {
+   const product = products.find((p) => p._id === req.params.id);
+   res.json(product);
+ });
+ 
+ app.get('/', (req, res) => {
+   res.send('API is running...');
+ });
+ 
+ app.listen(port, () => console.log(`Server running on port ${port}`));
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import { useEffect, useState } from 'react';  
 import { Row, Col } from 'react-bootstrap'; 
-import products from '../products';
-import Products from '../components/Products';
+import Product from '../components/Products'; 
+import axios from 'axios';
+// import products from '../products';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      
+        const { data } = await axios.get('/api/products');
+        setProducts(data);
+      };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Products product={product}/>
+            <Product product={product}/>
           </Col>
         ))}
       </Row>
