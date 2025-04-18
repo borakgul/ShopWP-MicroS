ce50fbc6e76ed3a1efe44be369cddc2bb725721d
Bora Akgül <borakgul1@gmail.com>
2025-04-18 16:42:17 +0300

    replace axios with Redux Toolkit queries for product data fetching both Home and product screens

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,37 +1,30 @@
-import { useEffect, useState } from 'react';  
 import { Row, Col } from 'react-bootstrap'; 
 import Product from '../components/Products'; 
+import { useGetProductsQuery } from '../slices/productSlice';
 
-import axios from 'axios';
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const { data } = await axios.get('/api/products');
-        console.log('Gelen veri:', data); // Burası konsola veri basmalı
-        setProducts(data);
-      } catch (error) {
-        console.error('Veri çekme hatası:', error.message);
-      }
-    };
-    fetchProducts();
-  }, []);
+ 
   
-  
-
+  const { data: products, isLoading, error } = useGetProductsQuery();
+ 
   return (
     <>
-      <h1>Latest Products</h1>
-
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
+     {isLoading ? (
+         <div>Loading...</div>
+       ) : error ? (
+         <div>{error?.data.message || error.error}</div>
+       ) : (
+         <>
+           <h1>Latest Products</h1>
+           <Row>
+             {products.map((product) => (
+               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                 <Product product={product} />
+               </Col>
+             ))}
+           </Row>
+         </>
+       )}
     </>
   );
 };
