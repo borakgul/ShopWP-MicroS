12dc0e4188126a8b67f542096b1a72e29933f857
Bora Akgül <borakgul1@gmail.com>
2025-04-20 23:14:01 +0300

    add logout functionality to user controller and routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,6 +39,16 @@ const authUser = asyncHandler(async (req, res) => {
 const registerUser = asyncHandler(async (req, res) => {
     res.send('register user');
 });
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
+};
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
@@ -85,6 +95,7 @@ const updateUser = asyncHandler(async (req, res) => {
 export {
     authUser,
     registerUser,
+    logoutUser,
     getUserProfile,
     updateUserProfile,
     getUsers,
